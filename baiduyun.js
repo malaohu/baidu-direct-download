d4747f9c649558e89b0b591dad239a922a10279b
malaohu <re_ming.ma@hotmail.com>
2018-05-13 15:00:04 +0800

    Add files via upload

---
+++ baiduyun.js
@@ -0,0 +1,6 @@
+var _d = document;
+var _s = _d.createElement('script');
+_s.textContent = "Object.defineProperty(navigator,'platform',{get:function(){return '';}});";
+_d.documentElement.appendChild(_s);
+
+
